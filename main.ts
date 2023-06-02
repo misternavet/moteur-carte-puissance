23949cf75250d8559559dfe220ce576ed8e81677
misternavet <132678563+misternavet@users.noreply.github.com>
2023-06-02 14:36:12 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,27 @@
 radio.setGroup(22)
+let onoff = 0
+let accéleration = 204
 basic.forever(function () {
     if (input.buttonIsPressed(Button.A)) {
         motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 204)
         motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, 204)
-    } else if (input.buttonIsPressed(Button.A)) {
+        onoff = 1
+    } else if (input.buttonIsPressed(Button.B)) {
         motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, 204)
         motor.MotorRun(motor.Motors.M2, motor.Dir.CW, 204)
+        onoff = 1
     } else {
-        motor.motorStopAll()
+        if (onoff == 1) {
+            accéleration = 204
+            for (let index = 0; index < 204; index++) {
+                basic.pause(10)
+                motor.MotorRun(motor.Motors.M1, motor.Dir.CW, accéleration)
+                motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, accéleration)
+                accéleration += -1
+            }
+            onoff = 0
+        } else {
+            motor.motorStopAll()
+        }
     }
 })
