3c95947168018e683c4066d8b09e02506e65b9e9
misternavet <132678563+misternavet@users.noreply.github.com>
2023-06-08 16:43:31 +0200

    Mise à jour main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
-def on_received_number(receivedNumber):
-    pass
-radio.on_received_number(on_received_number)
-
 def on_received_string(receivedString):
-    global onoff, déccélération
+    global onoff, accéleration, boost, déccélération
     if receivedString == "Accelere":
-        motor.motor_run(motor.Motors.M1, motor.Dir.CCW, 204)
-        motor.motor_run(motor.Motors.M2, motor.Dir.CCW, 204)
+        motor.motor_run(motor.Motors.M1, motor.Dir.CCW, accéleration)
+        motor.motor_run(motor.Motors.M2, motor.Dir.CCW, accéleration)
         onoff = 1
     elif receivedString == "reculer":
-        motor.motor_run(motor.Motors.M1, motor.Dir.CW, 204)
-        motor.motor_run(motor.Motors.M2, motor.Dir.CW, 204)
+        motor.motor_run(motor.Motors.M1, motor.Dir.CW, 180)
+        motor.motor_run(motor.Motors.M2, motor.Dir.CW, 180)
         onoff = 2
     elif receivedString == "Gauche":
-        if onoff == 1:
+        if onoff == 1 or onoff == 3:
             motor.motor_run(motor.Motors.M1, motor.Dir.CCW, 80)
             motor.motor_run(motor.Motors.M2, motor.Dir.CCW, 255)
         elif onoff == 2:
@@ -22,7 +18,7 @@ def on_received_string(receivedString):
         else:
             motor.motor_stop_all()
     elif receivedString == "Droite":
-        if onoff == 1:
+        if onoff == 1 or onoff == 3:
             motor.motor_run(motor.Motors.M1, motor.Dir.CCW, 255)
             motor.motor_run(motor.Motors.M2, motor.Dir.CCW, 80)
         elif onoff == 2:
@@ -31,13 +27,11 @@ def on_received_string(receivedString):
         else:
             motor.motor_stop_all()
     elif receivedString == "Boost":
-        motor.motor_run(motor.Motors.M1, motor.Dir.CCW, 255)
-        motor.motor_run(motor.Motors.M2, motor.Dir.CCW, 255)
-    elif receivedString == "Frein":
-        onoff = 0
+        accéleration = 255
+        boost = 1
     else:
         if onoff == 1 and receivedString != "Boost":
-            déccélération = 204
+            déccélération = 180
             for index in range(204):
                 motor.motor_run(motor.Motors.M1, motor.Dir.CCW, déccélération)
                 motor.motor_run(motor.Motors.M2, motor.Dir.CCW, déccélération)
@@ -45,13 +39,80 @@ def on_received_string(receivedString):
                 déccélération += -1
             onoff = 0
         else:
+            onoff = 0
             motor.motor_stop_all()
 radio.on_received_string(on_received_string)
 
+def invincibilité_musique():
+    music.play_tone(523, music.beat(BeatFraction.QUARTER))
+    music.rest(music.beat(BeatFraction.EIGHTH))
+    music.play_tone(523, music.beat(BeatFraction.EIGHTH))
+    music.rest(music.beat(BeatFraction.EIGHTH))
+    music.play_tone(523, music.beat(BeatFraction.HALF))
+    music.rest(music.beat(BeatFraction.EIGHTH))
+    music.play_tone(523, music.beat(BeatFraction.QUARTER))
+    music.rest(music.beat(BeatFraction.EIGHTH))
+    music.play_tone(523, music.beat(BeatFraction.EIGHTH))
+    music.rest(music.beat(BeatFraction.EIGHTH))
+    music.play_tone(523, music.beat(BeatFraction.EIGHTH))
+    music.rest(music.beat(BeatFraction.EIGHTH))
+    music.play_tone(523, music.beat(BeatFraction.EIGHTH))
+    music.rest(music.beat(BeatFraction.EIGHTH))
+    basic.pause(100)
+    music.rest(music.beat(BeatFraction.EIGHTH))
+    music.play_tone(494, music.beat(BeatFraction.QUARTER))
+    music.rest(music.beat(BeatFraction.EIGHTH))
+    music.play_tone(494, music.beat(BeatFraction.EIGHTH))
+    music.rest(music.beat(BeatFraction.EIGHTH))
+    music.play_tone(494, music.beat(BeatFraction.HALF))
+    music.rest(music.beat(BeatFraction.EIGHTH))
+    music.play_tone(494, music.beat(BeatFraction.QUARTER))
+    music.rest(music.beat(BeatFraction.EIGHTH))
+    music.play_tone(494, music.beat(BeatFraction.EIGHTH))
+    music.rest(music.beat(BeatFraction.EIGHTH))
+    music.play_tone(494, music.beat(BeatFraction.EIGHTH))
+    music.rest(music.beat(BeatFraction.EIGHTH))
+    music.play_tone(494, music.beat(BeatFraction.EIGHTH))
+    music.rest(music.beat(BeatFraction.EIGHTH))
+    basic.pause(100)
+boost = 0
+accéleration = 0
 déccélération = 0
 onoff = 0
+music.set_volume(255)
 radio.set_transmit_power(7)
 radio.set_group(33)
 onoff = 0
 déccélération = 204
-accéleration = 0
+accéleration = 180
+
+def on_forever():
+    if onoff == 1 and boost == 0:
+        music.play_sound_effect(music.create_sound_effect(WaveShape.SQUARE,
+                196,
+                196,
+                255,
+                255,
+                100,
+                SoundExpressionEffect.VIBRATO,
+                InterpolationCurve.LINEAR),
+            SoundExpressionPlayMode.UNTIL_DONE)
+basic.forever(on_forever)
+
+def on_forever2():
+    global accéleration, boost
+    if boost == 1:
+        basic.show_leds("""
+            . . # . .
+                        . . # . .
+                        # # # # #
+                        . # # # .
+                        # . . . #
+        """)
+        for index2 in range(6):
+            invincibilité_musique()
+        accéleration = 180
+        boost = 0
+    else:
+        basic.show_string("mon nom BOBERT")
+basic.forever(on_forever2)
--- main.ts
+++ main.ts
@@ -1,17 +1,14 @@
-radio.onReceivedNumber(function (receivedNumber) {
-	
-})
 radio.onReceivedString(function (receivedString) {
     if (receivedString == "Accelere") {
-        motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, 204)
-        motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, 204)
+        motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, accéleration)
+        motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, accéleration)
         onoff = 1
     } else if (receivedString == "reculer") {
-        motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 204)
-        motor.MotorRun(motor.Motors.M2, motor.Dir.CW, 204)
+        motor.MotorRun(motor.Motors.M1, motor.Dir.CW, 180)
+        motor.MotorRun(motor.Motors.M2, motor.Dir.CW, 180)
         onoff = 2
     } else if (receivedString == "Gauche") {
-        if (onoff == 1) {
+        if (onoff == 1 || onoff == 3) {
             motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, 80)
             motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, 255)
         } else if (onoff == 2) {
@@ -21,7 +18,7 @@ radio.onReceivedString(function (receivedString) {
             motor.motorStopAll()
         }
     } else if (receivedString == "Droite") {
-        if (onoff == 1) {
+        if (onoff == 1 || onoff == 3) {
             motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, 255)
             motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, 80)
         } else if (onoff == 2) {
@@ -31,29 +28,92 @@ radio.onReceivedString(function (receivedString) {
             motor.motorStopAll()
         }
     } else if (receivedString == "Boost") {
-        motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, 255)
-        motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, 255)
-    } else if (receivedString == "Frein") {
-        onoff = 0
+        accéleration = 255
+        boost = 1
     } else {
         if (onoff == 1 && receivedString != "Boost") {
-            déccélération = 204
-            for (let index = 0; index < 204; index++) {
-                motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, déccélération)
-                motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, déccélération)
-                basic.pause(2)
-                déccélération += -1
+            déccélération = 180
+            for (let index = 0; index < 180; index++) {
+                if (receivedString == "Frein") {
+                    motor.motorStopAll()
+                    onoff = 0
+                } else {
+                    motor.MotorRun(motor.Motors.M1, motor.Dir.CCW, déccélération)
+                    motor.MotorRun(motor.Motors.M2, motor.Dir.CCW, déccélération)
+                    basic.pause(2)
+                    déccélération += -1
+                }
             }
             onoff = 0
         } else {
+            onoff = 0
             motor.motorStopAll()
         }
     }
 })
+function invincibilité_musique () {
+    music.playTone(523, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.playTone(523, music.beat(BeatFraction.Eighth))
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.playTone(523, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.playTone(523, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.playTone(523, music.beat(BeatFraction.Eighth))
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.playTone(523, music.beat(BeatFraction.Eighth))
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.playTone(523, music.beat(BeatFraction.Eighth))
+    music.rest(music.beat(BeatFraction.Eighth))
+    basic.pause(100)
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.playTone(494, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.playTone(494, music.beat(BeatFraction.Eighth))
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.playTone(494, music.beat(BeatFraction.Half))
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.playTone(494, music.beat(BeatFraction.Quarter))
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.playTone(494, music.beat(BeatFraction.Eighth))
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.playTone(494, music.beat(BeatFraction.Eighth))
+    music.rest(music.beat(BeatFraction.Eighth))
+    music.playTone(494, music.beat(BeatFraction.Eighth))
+    music.rest(music.beat(BeatFraction.Eighth))
+    basic.pause(100)
+}
+let boost = 0
+let accéleration = 0
 let déccélération = 0
 let onoff = 0
+music.setVolume(255)
 radio.setTransmitPower(7)
 radio.setGroup(33)
 onoff = 0
 déccélération = 204
-let accéleration = 0
+accéleration = 180
+basic.forever(function () {
+    if (onoff == 1 && boost == 0) {
+        music.playSoundEffect(music.createSoundEffect(WaveShape.Square, 196, 196, 255, 255, 100, SoundExpressionEffect.Vibrato, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+    }
+})
+basic.forever(function () {
+    if (boost == 1) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # # # # #
+            . # # # .
+            # . . . #
+            `)
+        for (let index = 0; index < 6; index++) {
+            invincibilité_musique()
+        }
+        accéleration = 180
+        boost = 0
+    } else {
+        basic.showString("mon nom BOBERT")
+    }
+})
